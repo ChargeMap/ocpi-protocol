440eb694a518b7dbfbdb5bd51e41aca47ccd68f2
mikhail <mikhail@chargemap.com>
2020-08-03 15:39:55 +0200

    Add get requests and responses

--- src/Versions/V2_1_1/Server/Emsp/Locations/OcpiLocationUpdateRequest.php
+++ src/Versions/V2_1_1/Server/Emsp/Locations/OcpiLocationUpdateRequest.php
@@ -7,32 +7,11 @@ use Psr\Http\Message\RequestInterface;
 
 abstract class OcpiLocationUpdateRequest extends OcpiUpdateRequest
 {
-    protected string $countryCode;
-
-    protected string $partyId;
-
-    protected string $locationId;
+    use LocationRequestTrait;
 
     protected function __construct(RequestInterface $request, LocationRequestParams $params)
     {
         parent::__construct($request);
-        $this->countryCode = $params->getCountryCode();
-        $this->partyId = $params->getPartyId();
-        $this->locationId = $params->getLocationId();
-    }
-
-    public function getCountryCode(): string
-    {
-        return $this->countryCode;
-    }
-
-    public function getPartyId(): string
-    {
-        return $this->partyId;
-    }
-
-    public function getLocationId(): string
-    {
-        return $this->locationId;
+        $this->dispatchParams($params);
     }
 }
