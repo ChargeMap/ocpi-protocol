08390326360ad35687ed8043ec5d93c90c42f1d8
Gaydamakha <mikhail@chargemap.com>
2020-10-23 10:27:00 +0200

    Fix forging uri and test it

--- src/Common/Client/Modules/AbstractFeatures.php
+++ src/Common/Client/Modules/AbstractFeatures.php
@@ -6,6 +6,8 @@ namespace Chargemap\OCPI\Common\Client\Modules;
 
 use Chargemap\OCPI\Common\Client\OcpiConfiguration;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\UriInterface;
 
 class AbstractFeatures
 {
@@ -18,25 +20,38 @@ class AbstractFeatures
 
     protected function sendRequest(AbstractRequest $request): ResponseInterface
     {
-        $endpointUrl = $this->ocpiConfiguration->getEndpoint($request->getModule(), $request->getVersion())->getUri();
-
-        $serverRequestInterface = $request->getServerRequestInterface($this->ocpiConfiguration->getServerRequestFactory(), $this->ocpiConfiguration->getStreamFactory());
-
-        $uri = $serverRequestInterface->getUri()
-            ->withPath($endpointUrl->getPath())
-            ->withScheme($endpointUrl->getScheme())
-            ->withHost($endpointUrl->getHost());
-
-        if (!empty($endpointUrl->getPort())) {
-            $uri = $uri->withPort($endpointUrl->getPort());
-        }
-
-        if (!empty($endpointUrl->getUserInfo())) {
-            $uri = $uri->withUserInfo($endpointUrl->getUserInfo());
-        }
-
-        $serverRequestInterface = $serverRequestInterface->withUri($uri)->withHeader('Authorization', 'Token ' . $this->ocpiConfiguration->getToken());
+        $serverRequestInterface = $this->getServerRequestInterface($request);
         return $this->ocpiConfiguration->getHttpClient()->sendRequest($serverRequestInterface);
     }
 
+    private function getServerRequestInterface(AbstractRequest $request): ServerRequestInterface
+    {
+        $endpointUri = $this->ocpiConfiguration->getEndpoint($request->getModule(), $request->getVersion())->getUri();
+
+        $serverRequestInterface = $request->getServerRequestInterface($this->ocpiConfiguration->getServerRequestFactory(),
+            $this->ocpiConfiguration->getStreamFactory());
+
+        $uri = self::forgeUri($endpointUri, $serverRequestInterface->getUri());
+
+        return $serverRequestInterface->withUri($uri)
+            ->withHeader('Authorization', 'Token ' . $this->ocpiConfiguration->getToken());
+    }
+
+    private static function forgeUri(UriInterface $baseUri, UriInterface $requestUri): UriInterface
+    {
+        $uri = $requestUri
+            ->withPath($baseUri->getPath() . $requestUri->getPath())
+            ->withScheme($baseUri->getScheme())
+            ->withHost($baseUri->getHost());
+
+        if (!empty($baseUri->getPort())) {
+            $uri = $uri->withPort($baseUri->getPort());
+        }
+
+        if (!empty($baseUri->getUserInfo())) {
+            $uri = $uri->withUserInfo($baseUri->getUserInfo());
+        }
+
+        return $uri;
+    }
 }
