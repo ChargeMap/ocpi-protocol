4a9999673332394b5812a9e434d6593fb80731d3
Mathieu Muller <mathieu.muller@chargemap.com>
2020-11-04 10:05:16 +0100

    Fix CDR post unit test to be more strict with OCPI definition

--- tests/Versions/V2_1_1/Server/Emsp/Cdrs/Post/RequestConstructionTest.php
+++ tests/Versions/V2_1_1/Server/Emsp/Cdrs/Post/RequestConstructionTest.php
@@ -42,46 +42,57 @@ class RequestConstructionTest extends OcpiTestCase
         $this->assertEquals($json->auth_method, $cdr->getAuthMethod());
         $this->assertEquals($json->location->id, $cdr->getLocation()->getId());
         $this->assertEquals($json->currency, $cdr->getCurrency());
-        $this->assertCount(count($json->tariffs), $cdr->getTariffs());
 
-        $jsonTariff = $json->tariffs[0];
-        $tariff = $cdr->getTariffs()[0];
-        $this->assertEquals($jsonTariff->id, $tariff->getId());
-        $this->assertEquals($jsonTariff->currency, $tariff->getCurrency());
-        $this->assertEquals(new DateTime($jsonTariff->last_updated), $tariff->getLastUpdated());
-        $this->assertCount(count($jsonTariff->tariff_alt_text), $tariff->getTariffAltText());
-        $this->assertEquals($jsonTariff->tariff_alt_url, $tariff->getTariffAltUrl());
-        $this->assertCount(count($jsonTariff->elements), $tariff->getElements());
+        if (isset($json->tariffs)) {
 
-        $jsonElement = $jsonTariff->elements[0];
-        $element = $tariff->getElements()[0];
-        $this->assertCount(count($jsonElement->price_components), $element->getPriceComponents());
+            $this->assertCount(count($json->tariffs), $cdr->getTariffs());
 
-        $jsonPriceComponent = $jsonElement->price_components[0];
-        $priceComponent = $element->getPriceComponents()[0];
-        $this->assertEquals($jsonPriceComponent->type, $priceComponent->getType()->getValue());
-        $this->assertEquals($jsonPriceComponent->price, $priceComponent->getPrice());
-        $this->assertEquals($jsonPriceComponent->step_size, $priceComponent->getStepSize());
+            foreach( $json->tariffs as $tariffIndex => $jsonTariff) {
+                $tariff = $cdr->getTariffs()[$tariffIndex];
+                $this->assertEquals($jsonTariff->id, $tariff->getId());
+                $this->assertEquals($jsonTariff->currency, $tariff->getCurrency());
+                $this->assertEquals(new DateTime($jsonTariff->last_updated), $tariff->getLastUpdated());
+                $this->assertCount(count($jsonTariff->tariff_alt_text ?? []), $tariff->getTariffAltText());
+                $this->assertEquals($jsonTariff->tariff_alt_url ?? null, $tariff->getTariffAltUrl());
 
-        $jsonRestrictions = $jsonElement->restrictions;
+                $this->assertCount(count($jsonTariff->elements), $tariff->getElements());
 
-        if($jsonRestrictions !== null) {
-            $restrictions = $element->getRestrictions();
-            $this->assertEquals($jsonRestrictions->start_time ?? null, $restrictions->getStartTime());
-            $this->assertEquals($jsonRestrictions->end_time ?? null, $restrictions->getEndTime());
-            $this->assertEquals($jsonRestrictions->start_date ?? null, $restrictions->getStartDate());
-            $this->assertEquals($jsonRestrictions->end_date ?? null, $restrictions->getEndDate());
-            $this->assertEquals($jsonRestrictions->min_kwh ?? null, $restrictions->getMinKwh());
-            $this->assertEquals($jsonRestrictions->max_kwh ?? null, $restrictions->getMaxKwh());
-            $this->assertEquals($jsonRestrictions->min_power ?? null, $restrictions->getMinPower());
-            $this->assertEquals($jsonRestrictions->max_power ?? null, $restrictions->getMaxPower());
-            $this->assertEquals($jsonRestrictions->min_duration ?? null, $restrictions->getMinDuration());
-            $this->assertEquals($jsonRestrictions->max_duration ?? null, $restrictions->getMaxDuration());
-            $this->assertCount(count($jsonRestrictions->day_of_week ?? []), $restrictions->getDaysOfWeek());
+                foreach($jsonTariff->elements as $elementIndex => $jsonElement) {
 
-            $jsonDay = $jsonRestrictions->day_of_week[0];
-            $day = $restrictions->getDaysOfWeek()[0];
-            $this->assertEquals($jsonDay, $day->getValue());
+                    $element = $tariff->getElements()[$elementIndex];
+                    $this->assertCount(count($jsonElement->price_components), $element->getPriceComponents());
+
+                    foreach($jsonElement->price_components as $priceComponentIndex => $jsonPriceComponent) {
+                        $priceComponent = $element->getPriceComponents()[$priceComponentIndex];
+                        $this->assertEquals($jsonPriceComponent->type, $priceComponent->getType()->getValue());
+                        $this->assertEquals($jsonPriceComponent->price, $priceComponent->getPrice());
+                        $this->assertEquals($jsonPriceComponent->step_size, $priceComponent->getStepSize());
+                    }
+
+                    $jsonRestrictions = $jsonElement->restrictions ?? null;
+
+                    if ($jsonRestrictions !== null) {
+                        $restrictions = $element->getRestrictions();
+                        $this->assertEquals($jsonRestrictions->start_time ?? null, $restrictions->getStartTime());
+                        $this->assertEquals($jsonRestrictions->end_time ?? null, $restrictions->getEndTime());
+                        $this->assertEquals($jsonRestrictions->start_date ?? null, $restrictions->getStartDate());
+                        $this->assertEquals($jsonRestrictions->end_date ?? null, $restrictions->getEndDate());
+                        $this->assertEquals($jsonRestrictions->min_kwh ?? null, $restrictions->getMinKwh());
+                        $this->assertEquals($jsonRestrictions->max_kwh ?? null, $restrictions->getMaxKwh());
+                        $this->assertEquals($jsonRestrictions->min_power ?? null, $restrictions->getMinPower());
+                        $this->assertEquals($jsonRestrictions->max_power ?? null, $restrictions->getMaxPower());
+                        $this->assertEquals($jsonRestrictions->min_duration ?? null, $restrictions->getMinDuration());
+                        $this->assertEquals($jsonRestrictions->max_duration ?? null, $restrictions->getMaxDuration());
+                        $this->assertCount(count($jsonRestrictions->day_of_week ?? []), $restrictions->getDaysOfWeek());
+
+                        $jsonDay = $jsonRestrictions->day_of_week[0];
+                        $day = $restrictions->getDaysOfWeek()[0];
+                        $this->assertEquals($jsonDay, $day->getValue());
+                    }
+                }
+            }
+        } else {
+            $this->assertCount(0, $cdr->getTariffs());
         }
     }
 
