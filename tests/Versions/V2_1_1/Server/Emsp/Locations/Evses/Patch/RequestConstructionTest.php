8c2a1ee44c25933919549796cdebccd777a79d91
Thomas Steinmetz <thomas.steinmetz@chargemap.com>
2021-01-06 14:16:01 +0100

    fix after feedback

--- tests/Versions/V2_1_1/Server/Emsp/Locations/Evses/Patch/RequestConstructionTest.php
+++ tests/Versions/V2_1_1/Server/Emsp/Locations/Evses/Patch/RequestConstructionTest.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace Tests\Chargemap\OCPI\Versions\V2_1_1\Server\Emsp\Locations\Evses\Patch;
 
-use Chargemap\OCPI\Versions\V2_1_1\Common\Models\GeoLocation;
 use Chargemap\OCPI\Versions\V2_1_1\Server\Emsp\Locations\Evses\Patch\OcpiEmspEvsePatchRequest;
 use Chargemap\OCPI\Versions\V2_1_1\Server\Emsp\Locations\LocationRequestParams;
 use DateTime;
-use stdClass;
 use Tests\Chargemap\OCPI\OcpiTestCase;
+use Tests\Chargemap\OCPI\Versions\V2_1_1\Common\Factories\ConnectorFactoryTest;
+use Tests\Chargemap\OCPI\Versions\V2_1_1\Common\Factories\GeoLocationFactoryTest;
+use Tests\Chargemap\OCPI\Versions\V2_1_1\Common\Factories\ImageFactoryTest;
 
 class RequestConstructionTest extends OcpiTestCase
 {
@@ -35,75 +36,65 @@ class RequestConstructionTest extends OcpiTestCase
         $json = json_decode(file_get_contents($filename));
 
         $request = new OcpiEmspEvsePatchRequest($serverRequestInterface, new LocationRequestParams('FR', 'TNM', 'LOC1', '3256'));
-        $this->assertEquals('FR', $request->getCountryCode());
-        $this->assertEquals('TNM', $request->getPartyId());
-        $this->assertEquals('LOC1', $request->getLocationId());
-        $this->assertEquals('3256', $request->getEvseUid());
+        $this->assertSame('FR', $request->getCountryCode());
+        $this->assertSame('TNM', $request->getPartyId());
+        $this->assertSame('LOC1', $request->getLocationId());
+        $this->assertSame('3256', $request->getEvseUid());
 
         $evse = $request->getPartialEvse();
-        if ($evse->getUid() !== null) {
-            $this->assertEquals($json->uid, $evse->getUid());
-        }
-        if ($evse->getEvseId() !== null) {
-            $this->assertEquals($json->evse_id, $evse->getEvseId());
-        }
-        if ($evse->getStatus() !== null) {
-            $this->assertEquals($json->status, $evse->getStatus()->getValue());
-        }
-        if ($evse->getStatusSchedule() !== null) {
-            $this->assertCount(count($json->status_schedule), $evse->getStatusSchedule());
+        $this->assertSame($json->uid ?? null, $evse->getUid());
+        $this->assertSame($json->evse_id ?? null, $evse->getEvseId());
+        $this->assertEquals($json->status ?? null, $evse->getStatus());
+        if (isset($json->status_schedule)) {
+            $this->assertCount(count($json->status_schedule ?? []), $evse->getStatusSchedule());
             foreach ($evse->getStatusSchedule() as $index => $statusSchedule) {
                 $this->assertEquals(new DateTime($json->status_schedule[$index]->period_begin), $statusSchedule->getPeriodBegin());
                 $this->assertEquals(new DateTime($json->status_schedule[$index]->period_end), $statusSchedule->getPeriodEnd());
-                $this->assertEquals($json->status_schedule[$index]->status, $statusSchedule->getStatus()->getValue());
+                $this->assertSame($json->status_schedule[$index]->status, $statusSchedule->getStatus()->getValue());
             }
         }
-        if ($evse->getDirections() !== null) {
-            $this->assertCount(count($json->directions), $evse->getDirections());
+        if (isset($json->directions)) {
+            $this->assertCount(count($json->directions ?? []), $evse->getDirections());
             foreach ($evse->getDirections() as $index => $direction) {
-                $this->assertEquals($json->directions[$index]->language, $direction->getLanguage());
-                $this->assertEquals($json->directions[$index]->text, $direction->getText());
+                $this->assertSame($json->directions[$index]->language, $direction->getLanguage());
+                $this->assertSame($json->directions[$index]->text, $direction->getText());
             }
         }
-        if ($evse->getParkingRestrictions() !== null) {
-            $this->assertCount(count($json->parking_restrictions), $evse->getParkingRestrictions());
+        if (isset($json->parking_restrictions)) {
+            $this->assertCount(count($json->parking_restrictions ?? []), $evse->getParkingRestrictions());
             foreach ($evse->getParkingRestrictions() as $index => $parkingRestriction) {
-                $this->assertEquals($json->parking_restrictions[$index], $parkingRestriction->getValue());
+                $this->assertSame($json->parking_restrictions[$index], $parkingRestriction->getValue());
             }
         }
-        if ($evse->getCoordinates() !== null) {
-            $this->assertEquals(new GeoLocation($json->coordinates->latitude, $json->coordinates->longitude), $evse->getCoordinates());
-        }
-        if ($evse->getCapabilities() !== null) {
+        GeoLocationFactoryTest::assertGeolocation($json->coordinates, $evse->getCoordinates());
+        if (isset($json->capabilities)) {
             $this->assertCount(count($json->capabilities), $evse->getCapabilities());
             foreach ($evse->getCapabilities() as $index => $capability) {
                 $this->assertEquals($json->capabilities[$index], $capability);
             }
         }
-        if ($evse->getConnectors() !== null) {
-            self::assertConnectors($json, $request);
+        if (isset($json->connectors)) {
+            self::assertCount(count($json->connectors), $evse->getConnectors());
+            foreach ($evse->getConnectors() as $index => $connector) {
+                ConnectorFactoryTest::assertConnector($json->connectors[$index],$connector);
+            }
         }
-        if ($evse->getPhysicalReference() !== null) {
-            $this->assertEquals($json->physical_reference, $evse->getPhysicalReference());
-        }
-        if ($evse->getFloorLevel() !== null) {
-            $this->assertEquals($json->floor_level, $evse->getFloorLevel());
-        }
-        if ($evse->getImages() !== null) {
-            $this->assertCount(count($json->images), $evse->getImages());
-        }
-        if ($evse->getLastUpdated() !== null) {
-            $this->assertEquals(new DateTime($json->last_updated), $evse->getLastUpdated());
-        }
-    }
+        $this->assertSame($json->physical_reference ?? null, $evse->getPhysicalReference());
+        $this->assertSame($json->floor_level ?? null, $evse->getFloorLevel());
 
-    public static function assertConnectors(stdClass $json, OcpiEmspEvsePatchRequest $request)
-    {
-        $partialEvse = $request->getPartialEvse();
-        self::assertCount(count($json->connectors), $partialEvse->getConnectors());
+        if (isset($json->images)) {
+            $this->assertCount(count($json->images ?? []), $evse->getImages());
+            foreach ($evse->getImages() as $index => $image){
+                ImageFactoryTest::assertImage($json->images[$index],$image);
+            }
+        }
 
-        foreach ($partialEvse->getConnectors() as $index => $value) {
-            self::assertEquals($json->connectors[$index]->id, $value->getId());
+        if (property_exists($json,'last_updated')) {
+            if(($json->last_updated ?? null) === null){
+                $this->assertNull($evse->getLastUpdated());
+            } else {
+                $this->assertEquals(new DateTime($json->last_updated), $evse->getLastUpdated());
+            }
         }
     }
 }
