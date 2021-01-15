55183caa326dc9a4c43151ee3d6b5e85d149715e
Mathieu Muller <mathieu.muller@chargemap.com>
2021-01-15 10:58:08 +0100

    Add Unauthorized and Forbidden in Ocpi error codes.

--- src/Common/Server/StatusCodes/OcpiErrorHttpCode.php
+++ src/Common/Server/StatusCodes/OcpiErrorHttpCode.php
@@ -6,12 +6,16 @@ namespace Chargemap\OCPI\Common\Server\StatusCodes;
 
 /**
  * @method static self HTTP_BAD_REQUEST()
+ * @method static self HTTP_UNAUTHORIZED()
+ * @method static self HTTP_FORBIDDEN()
  * @method static self HTTP_NOT_FOUND()
  * @method static self HTTP_METHOD_NOT_ALLOWED()
  */
 class OcpiErrorHttpCode extends OcpiHttpCode
 {
     public const HTTP_BAD_REQUEST = 400;
+    public const HTTP_UNAUTHORIZED = 401;
+    public const HTTP_FORBIDDEN = 403;
     public const HTTP_NOT_FOUND = 404;
     public const HTTP_METHOD_NOT_ALLOWED = 405;
 }
