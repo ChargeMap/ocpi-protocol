0994fd3841e8b0f90e0e20597a2713a494c3657b
Thomas Steinmetz <thomas.steinmetz@chargemap.com>
2021-03-11 14:32:32 +0100

    fix tests

--- tests/Versions/V2_1_1/Common/Factories/PriceComponentsFactoryTest.php
+++ tests/Versions/V2_1_1/Common/Factories/PriceComponentsFactoryTest.php
@@ -3,15 +3,15 @@
 namespace Tests\Chargemap\OCPI\Versions\V2_1_1\Common\Factories;
 
 use Chargemap\OCPI\Versions\V2_1_1\Common\Models\PriceComponent;
-use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Assert;
 use stdClass;
 
-class PriceComponentsFactoryTest extends TestCase
+class PriceComponentsFactoryTest
 {
     public static function assertPriceComponents(StdClass $json, PriceComponent $priceComponent): void
     {
-        self::assertSame($json->type, $priceComponent->getType()->getValue());
-        self::assertSame((float)$json->price, $priceComponent->getPrice());
-        self::assertSame((int)$json->step_size, $priceComponent->getStepSize());
+        Assert::assertSame($json->type, $priceComponent->getType()->getValue());
+        Assert::assertSame((float)$json->price, $priceComponent->getPrice());
+        Assert::assertSame((int)$json->step_size, $priceComponent->getStepSize());
     }
 }
--- tests/Versions/V2_1_1/Common/Factories/TariffRestrictionsFactoryTest.php
+++ tests/Versions/V2_1_1/Common/Factories/TariffRestrictionsFactoryTest.php
@@ -3,33 +3,33 @@
 namespace Tests\Chargemap\OCPI\Versions\V2_1_1\Common\Factories;
 
 use Chargemap\OCPI\Versions\V2_1_1\Common\Models\TariffRestrictions;
-use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Assert;
 use stdClass;
 
-class TariffRestrictionsFactoryTest extends TestCase
+class TariffRestrictionsFactoryTest
 {
     public static function assertTariffRestrictions(?stdClass $json, ?TariffRestrictions $tariffRestrictions): void
     {
         if($json === null){
-            self::assertNull($tariffRestrictions);
+            Assert::assertNull($tariffRestrictions);
         } else {
-            self::assertSame($json->start_time ?? null, $tariffRestrictions->getStartTime());
-            self::assertSame($json->end_time ?? null, $tariffRestrictions->getEndTime());
-            self::assertSame($json->start_date ?? null, $tariffRestrictions->getStartDate());
-            self::assertSame($json->end_date ?? null, $tariffRestrictions->getEndDate());
-            self::assertSame((float)$json->min_kwh ?? null, $tariffRestrictions->getMinKwh());
-            self::assertSame((float)$json->max_kwh ?? null, $tariffRestrictions->getMaxKwh());
-            self::assertSame((float)$json->min_power ?? null, $tariffRestrictions->getMinPower());
-            self::assertSame((float)$json->max_power ?? null, $tariffRestrictions->getMaxPower());
-            self::assertSame((int)$json->min_duration ?? null, $tariffRestrictions->getMinDuration());
-            self::assertSame((int)$json->max_duration ?? null, $tariffRestrictions->getMaxDuration());
+            Assert::assertSame($json->start_time ?? null, $tariffRestrictions->getStartTime());
+            Assert::assertSame($json->end_time ?? null, $tariffRestrictions->getEndTime());
+            Assert::assertSame($json->start_date ?? null, $tariffRestrictions->getStartDate());
+            Assert::assertSame($json->end_date ?? null, $tariffRestrictions->getEndDate());
+            Assert::assertSame((float)$json->min_kwh ?? null, $tariffRestrictions->getMinKwh());
+            Assert::assertSame((float)$json->max_kwh ?? null, $tariffRestrictions->getMaxKwh());
+            Assert::assertSame((float)$json->min_power ?? null, $tariffRestrictions->getMinPower());
+            Assert::assertSame((float)$json->max_power ?? null, $tariffRestrictions->getMaxPower());
+            Assert::assertSame((int)$json->min_duration ?? null, $tariffRestrictions->getMinDuration());
+            Assert::assertSame((int)$json->max_duration ?? null, $tariffRestrictions->getMaxDuration());
 
             if (property_exists($json, 'day_of_week')) {
                 foreach ($tariffRestrictions->getDaysOfWeek() as $index => $dayOfWeek) {
-                    self::assertSame($json->day_of_week[$index], $dayOfWeek->getValue());
+                    Assert::assertSame($json->day_of_week[$index], $dayOfWeek->getValue());
                 }
             } else {
-                self::assertNull($tariffRestrictions->getDaysOfWeek());
+                Assert::assertNull($tariffRestrictions->getDaysOfWeek());
             }
         }
     }
